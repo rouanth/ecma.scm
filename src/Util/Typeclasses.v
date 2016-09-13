b96d89d68278c801e2e0ff84540cbb534a21d1dd
Dmitry Khalansky <rouanth@gmail.com>
2016-09-13 19:04:17 +0300

    Added multiple notations to type classes
    In Haskell present is a multitude of useful infix operators, making
    programs much more readable. Due to the fact that this work will require
    much parsing, it makes sense to sugar syntax descriptions to look as
    much alike to informal BNFs as possible.

--- src/Util/Typeclasses.v
+++ src/Util/Typeclasses.v
@@ -6,38 +6,53 @@ Module Typeclasses.
 
 Local Open Scope program_scope.
 
+Reserved Notation "f <$> a" (at level 40).
+
 Class Functor f := {
-  fmap {A B} : (A -> B) -> f A -> f B;
+  fmap {A B} : (A -> B) -> f A -> f B where "f <$> a" := (fmap f a);
   functor_hom : forall {A B C} (g : B -> C) (h : A -> B) x,
     fmap (g ∘ h) x = (fmap g ∘ fmap h) x;
   functor_id : forall A x, @fmap A A id x = id x
 }.
+
+Notation "f <$> a" := (fmap f a).
 
 Reserved Notation "a <*> b" (at level 40).
 
 Class Applicative f:= {
   applic_is_functor :> Functor f;
   pure : forall {A}, A -> f A;
-  comb : forall {A B}, f (A -> B) -> f A -> f B;
+  comb : forall {A B}, f (A -> B) -> f A -> f B where "a <*> b" := (comb a b);
   applic_id : forall {A} (x : f A), comb (pure id) x = x;
   applic_hom : forall {A B} (f : A -> B) x,
-    comb (pure f) (pure x) = pure (f x);
+    pure f <*> pure x = pure (f x);
   applic_inter : forall {A B} (u : f (A -> B)) y,
-    comb u (pure y) = comb (pure (fun f => f y)) u;
+    u <*> pure y = pure (fun f => f y) <*> u;
   applic_comp : forall {A B C} (u : f (B -> C)) (v : f (A -> B)) w,
-    comb u (comb v w) = comb (comb (comb (pure compose) u) v) w
+    u <*> comb v w = pure compose <*> u <*> v <*> w
 }.
+
+Notation "a <*> b" := (comb a b).
+
+Notation "a <$ b" := ((comb ∘ pure ∘ const) a b) (at level 40).
+
+Notation "a *> b" := ((id <$ a) <*> b) (at level 40).
+
+Notation "a <* b" := (const <$> a <*> b) (at level 40).
+
+Reserved Notation "a <|> b" (at level 30).
 
 Class Alternative f := {
   altern_is_applic :> Applicative f;
   altern_empty : forall {A}, f A;
-  altern_or    : forall {A}, f A -> f A -> f A;
-  altern_left_empty : forall {A} x,
-    @altern_or A altern_empty x = x;
-  altern_right_empty : forall {A} x,
-    @altern_or A x altern_empty = x;
-  altern_assoc : forall {A} x y z,
-    @altern_or A (altern_or x y) z = altern_or x (altern_or y z)
+  altern_or    : forall {A}, f A -> f A -> f A
+    where "a <|> b" := (altern_or a b);
+  altern_left_empty : forall {A} (x : f A),
+    altern_empty <|> x = x;
+  altern_right_empty : forall {A} (x : f A),
+    x <|> altern_empty = x;
+  altern_assoc : forall {A} (x y z : f A),
+    (x <|> y) <|> z = x <|> (y <|> z)
 }.
 
 Instance option_Functor : Functor option := {
